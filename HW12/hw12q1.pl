cebe30d8be98f03c20afdb72e2636da5323271d0
royakabiri <43491165+royakabiri@users.noreply.github.com>
2022-01-14 16:11:10 -0700

    Add files via upload

---
+++ HW12/hw12q1.pl
@@ -0,0 +1,28 @@
+eee --> [a], oee.
+eee --> [b], eoe.
+eee --> [c], eeo.
+eeo --> [a].
+eeo --> [a], oeo.
+eeo --> [b], eoo.
+eeo --> [c], eee.
+eoe --> [a], ooe.
+eoe --> [b], eee.
+eoe --> [c], eoo.
+eoo --> [a], ooo.
+eoo --> [b], eeo.
+eoo --> [c], eoe.
+oee --> [a], eee.
+oee --> [b], ooe.
+oee --> [c].
+oee --> [c], oeo.
+ooo --> [a], eoo.
+ooo --> [b].
+ooo --> [b], oeo.
+ooo --> [c], ooe.
+ooe --> [a], eoe.
+ooe --> [b], oee.
+ooe --> [c], ooo.
+oeo --> [a], eeo.
+oeo --> [b], ooo.
+oeo --> [c], oee.
+
